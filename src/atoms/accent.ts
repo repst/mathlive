b0439bd9b523d136619c947ae07b980b2482fc2f
Ryan Epstein <rjepstein@gmail.com>
2025-04-22 21:05:15 -0600

    Revert "Update accent.ts"
    This reverts commit 8a48f1f7f2d7edbcf444a2694b3f06301c1fb9d4.

--- src/atoms/accent.ts
+++ src/atoms/accent.ts
@@ -126,7 +126,7 @@ export class AccentAtom extends Atom {
       ],
     });
 
-    const result = new Box(accentBox, { type: 'ord' });   
+    const result = new Box(accentBox, { type: 'lift' });
     if (this.caret) result.caret = this.caret;
     this.bind(context, result.wrap(context));
     return this.attachSupsub(context, { base: result });
